f545ac47b8cdade3ce939e07231f36f114ef134d
arsaIan <arsalanrashidthoker@gmail.com>
2025-05-19 22:58:06 +0530

    add start script

---
+++ start.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+
+# Configuration
+host = "0.0.0.0"
+port = "5001"
+workers = "2"
+app_path = "app:app"  
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# logs
+access_log = os.path.join(script_dir, "access.log")
+error_log = os.path.join(script_dir, "error.log")
+
+
+cmd = [
+    "gunicorn",
+    app_path,
+    "--bind", f"{host}:{port}",
+    "--workers", workers,
+    "--daemon",
+    "--access-logfile", access_log,
+    "--error-logfile", error_log
+]
+
+try:
+    subprocess.run(cmd, check=True)
+    print(f"Gunicorn started on {host}:{port} in background.")
+    print(f"Logs: access → {access_log}, error → {error_log}")
+except subprocess.CalledProcessError as e:
+    print("❌ Failed to start Gunicorn:", e)
